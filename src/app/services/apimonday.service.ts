f29f4be1af3d71d3dcf7d04c8e91e0baa44c4de5
Cesar Martinez <131212427+Cesarmtz1@users.noreply.github.com>
2024-05-06 17:48:54 -0600

    Update apimonday.service.ts

--- src/app/services/apimonday.service.ts
+++ src/app/services/apimonday.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from 'src/evironments/environments';
+import { environment } from 'src/evironments/environment';
 @Injectable({
   providedIn: 'root'
 })
