c5a291c88433a7b80c54ca9a574d173ab2dbada6
Cesar Martinez <131212427+Cesarmtz1@users.noreply.github.com>
2024-05-06 14:39:43 -0600

    Update environments.ts

--- src/evironments/environments.ts
+++ src/evironments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  environment: 'development',
-  API_URL: "https://api-bdc-v1-prvfgsimwa-uc.a.run.app/",
-  API_KEY:'1B2D3C-?j',
+  environment: 'xxt',
+  API_URL: "xxxx",
+  API_KEY:'xxx',
 }
